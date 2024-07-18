ba3d507602346ac2f6e54ebee2b3f66122eada2a
TalyaNeppe <talyaneppe@hilma.tech>
2024-07-18 15:37:39 +0300

    add more sentences and pizza

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,15 @@ function App() {
     "we love you!",
     "eat trash",
     "Why are you here?",
+    "Do you think there are others out there?",
+    "MooOoo 🐄",
+    "Who let the dogs out?",
+    "What does the fox say?",
+    "Hey :D",
+    "Get out of here",
+    "...knock, knock?",
   ];
+
   return <h1>{sentences[Math.floor(Math.random() * sentences.length)]}</h1>;
 }
 
